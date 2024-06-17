134e797bae10025be94458baabae697f5812fec9
iamhritik <hritik.99@outlook.com>
2024-06-17 19:41:40 +0530

    create newDeck function - Day2

--- cards-project/deck.go
+++ cards-project/deck.go
@@ -7,6 +7,20 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck { // over here we are defining a func newDeck and it returns deck data type
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits { // creating nested for loops over and '_' when we don't want to use that variable in the code
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() { // works as a receiver and instead of print you can define any method like set, get
 	for i, card := range d {
 		fmt.Println(i, card)
--- cards-project/main.go
+++ cards-project/main.go
@@ -2,12 +2,13 @@ package main
 
 func main() {
 	//cards := []string{"Ace of Diamonds", newCard()} // type should be singular
-	cards := deck{"Ace of Diamonds", newCard()} // here we are using deck type which is a slice of strings
-	cards = append(cards, "Six of Spades")      // it will assign this value to this slice var and it's modifying the existing cards var but creating a new one
+	// cards := deck{"Ace of Diamonds", newCard()} // here we are using deck type which is a slice of strings
+	// cards = append(cards, "Six of Spades")      // it will assign this value to this slice var and it's modifying the existing cards var but creating a new one
 
 	// for i, card := range cards { // iterating cards slice var
 	// 	fmt.Println(i, card) // print index and it's value
 	// }
+	cards := newDeck()
 	cards.print()
 }
 
