259f53713966098ec1b36503991f6775a325216e
iamhritik <hritik.99@outlook.com>
2024-08-01 10:46:23 +0530

    struct with pointer and pointers overview

--- structs/main.go
+++ structs/main.go
@@ -29,6 +29,20 @@ func main() {
 	// fmt.Printf("%+v", employee) //it prints field namees along with their values | use for debugging & logging purpose
 
 	//embedding struct
+	// jim := person{
+	// 	firstName: "Hritik",
+	// 	lastName:  "kumar",
+	// 	contactInfo: contactInfo{
+	// 		email:   "hritik@github.com",
+	// 		zipCode: 11009999,
+	// 	},
+	// }
+	// fmt.Println(jim.contactInfo.email)
+	// fmt.Printf("%+v", jim)
+	// jim.updateName("Saul") // but it's not updating the main - jim struct firstName due to some Pointers logic
+	// jim.print()
+
+	// struct wth point
 	jim := person{
 		firstName: "Hritik",
 		lastName:  "kumar",
@@ -37,16 +51,22 @@ func main() {
 			zipCode: 11009999,
 		},
 	}
-	fmt.Println(jim.contactInfo.email)
-	fmt.Printf("%+v", jim)
-	jim.updateName("Saul") // but it's not updating the main - jim struct firstName due to some Pointers logic
+	// jimPointer := &jim            // give me access to the memory address of this jim struct and then assigned it to jimPointer variable
+	// jimPointer.updateName("Saul") // but it's not updating the main - jim struct firstName due to some Pointers logic
+
+	jim.updateName("Saul") // this will also works with the receiver pointer
 	jim.print()
+
 }
 
 func (p person) print() { // receiver function
 	fmt.Printf("%v", p) // %v - just values , %+v -> field name and value
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+// func (p person) updateName(newFirstName string) {
+// 	p.firstName = newFirstName
+// }
+
+func (pointerToPerson *person) updateName(newFirstName string) { // receiver func using pointers to update the original jim struct firstName
+	(*pointerToPerson).firstName = newFirstName //over here dereference not works if pointerToPerson is not pointer to person type
 }
