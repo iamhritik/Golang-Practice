bf45133dd3a3b0f37f78b74d6721ac3d79d9052a
iamhritik <hritik.99@outlook.com>
2024-08-16 00:24:49 +0530

    interface related code

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{} // custom interface
 
 func main() {
 	resp, err := http.Get("https://homelane.com")
@@ -13,5 +16,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	//fmt.Println(resp) //right now it;s a HTTP header response but not the whole body
+	// bs := make([]byte, 9999999) //creating byte slice with empty 9999 elements
+	// resp.Body.Read(bs)          // Read takes bytes slice and read the data and put it in the byte slice
+	// fmt.Println(string(bs))
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
